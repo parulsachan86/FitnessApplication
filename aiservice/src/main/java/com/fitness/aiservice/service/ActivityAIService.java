3d0e36e486930aadd60c0131de82b1ea2eca4994
Parul Sachan <parulsachan86@gmail.com>
2026-03-29 21:56:23 +0530

    Created ActivityAIService and parsed the Recommendation in correct format

--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityAIService.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityAIService.java
@@ -1,9 +1,20 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
+import com.fitness.aiservice.model.Recommendation;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 @Service
 @Slf4j
@@ -11,10 +22,80 @@ import org.springframework.stereotype.Service;
 public class ActivityAIService {
 
     private final GeminiService geminiService;
-    public String generateRecommendation(Activity activity) {
+    public Recommendation generateRecommendation(Activity activity) {
         String prompt = createPromptForActivity(activity);
         String aiRecommendation = geminiService.getRecommendationFromAI(prompt);
-        return aiRecommendation;
+        // System.out.println(aiRecommendation);
+        Recommendation recommendation =  parseAiRecommendation(aiRecommendation, activity);
+        return recommendation;
+    }
+
+    public List<String> jsonArrayToList(JsonArray array) {
+        List <String> list = new ArrayList<>();
+        for (JsonElement element : array) {
+            list.add(element.toString());
+        }
+        return list;
+    }
+
+    public Recommendation parseAiRecommendation (String aiRecommendation, Activity activity) {
+        try{
+            JsonObject jsonRecommendation = JsonParser.parseString(aiRecommendation).getAsJsonObject();
+            JsonArray candidates = jsonRecommendation.get("candidates").getAsJsonArray();
+            JsonObject candidate = candidates.get(0).getAsJsonObject();
+            JsonObject content = candidate.get("content").getAsJsonObject();
+            JsonArray parts = content.get("parts").getAsJsonArray();
+            JsonObject part = parts.get(0).getAsJsonObject();
+            String text = part.get("text").getAsString();
+
+            String cleanText = text
+                    .replace("```json", "")
+                    .replace("```", "")
+                    .trim();
+
+            JsonObject finalAIRecommendation = JsonParser.parseString(cleanText).getAsJsonObject();
+            System.out.println(finalAIRecommendation);
+
+            Recommendation recommendation = new Recommendation();
+            recommendation.setRecommendation(finalAIRecommendation.get("analysis").toString());
+
+            JsonArray improvements = finalAIRecommendation.get("improvements").getAsJsonArray();
+            recommendation.setImprovements(jsonArrayToList(improvements));
+
+
+            JsonArray suggestions = finalAIRecommendation.get("suggestions").getAsJsonArray();
+            recommendation.setSuggestions(jsonArrayToList(suggestions));
+
+            JsonArray safety = finalAIRecommendation.get("safety").getAsJsonArray();
+            recommendation.setSafety(jsonArrayToList(safety));
+
+            recommendation.setActivityId(activity.getId());
+            recommendation.setActivityType(activity.getType());
+            recommendation.setUserId(activity.getUserId());
+            recommendation.setCreatedAt(activity.getCreatedAt());
+            return recommendation;
+        }
+        catch (Exception e){
+            log.error("Error occurred in parsing the AIRecommendation response" + e.getMessage());
+            return createDefaultRecommendation(activity);
+        }
+    }
+
+    private Recommendation createDefaultRecommendation(Activity activity) {
+          return Recommendation.builder()
+                .activityId(activity.getId())
+                .userId(activity.getUserId())
+                .activityType(activity.getType())
+                .recommendation("Unable to generate detailed analysis")
+                .improvements(Collections.singletonList("Continue with your current routine"))
+                .suggestions(Collections.singletonList("Consider consulting a fitness professional"))
+                .safety(Arrays.asList(
+                        "Always warm up before exercise",
+                        "Stay hydrated",
+                        "Listen to your body"
+                ))
+                .createdAt(LocalDateTime.now())
+                .build();
     }
 
     private String createPromptForActivity(Activity activity) {
--- aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
+++ aiservice/src/main/java/com/fitness/aiservice/service/ActivityMessageListener.java
@@ -1,8 +1,8 @@
 package com.fitness.aiservice.service;
 
 import com.fitness.aiservice.model.Activity;
-import com.google.genai.Client;
-import com.google.genai.types.GenerateContentResponse;
+import com.fitness.aiservice.model.Recommendation;
+import com.fitness.aiservice.repository.RecommendationRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
@@ -14,14 +14,15 @@ import org.springframework.stereotype.Service;
 public class ActivityMessageListener {
 
     private final ActivityAIService activityAIService;
+    private final RecommendationRepository recommendationRepository;
 
     @RabbitListener(queues = "activity.queue")
     public void processActivity(Activity activity){
 
         log.info("Activity received from RabbitMQ" + activity);
-        String recommendation = activityAIService.generateRecommendation(activity);
+        Recommendation recommendation = activityAIService.generateRecommendation(activity);
         log.info("Recommendation generated from AI:" + recommendation);
-
+        recommendationRepository.save(recommendation);
     }
 
 }
